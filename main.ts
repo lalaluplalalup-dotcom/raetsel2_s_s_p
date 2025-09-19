c79bf1f5651246c30b9a38906e93552872529354
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2025-09-19 13:24:16 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 let zufall = 0
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showNumber(zufall)
+})
 input.onGesture(Gesture.Shake, function () {
     zufall += randint(1, 3)
     if (zufall == 1) {
